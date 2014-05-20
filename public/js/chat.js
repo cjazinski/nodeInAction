d2f541ea148e38b8e67f4816661bfd2a10f36d7f
Christopher Jazinski <cgjazinski@utpa.edu>
2014-05-20 13:58:05 -0500

    client-side chat.js and process commands

---
+++ public/js/chat.js
@@ -0,0 +1,44 @@
+//JS equivalent of a class
+
+var Chat = function(socket) {
+	this.socket = socket;
+}
+
+Chat.prototype.sendMessage = function (room, data) {
+	var messaage = {
+		room: room,
+		text: data
+	};
+	this.socket.emit('message', message);
+};
+
+Chat.prototype.changeRoom = function(room) {
+	this.socket.emit('join', {
+		newRoom: room
+	});
+};
+
+//Process the commands
+Chat.prototype.processCommand = function(command) {
+	var words = command.split(' ');
+	var command = words[0].substring(1, words[0].length).toLowerCase();
+	var message = false;
+
+	//Switch the commands
+	switch(command) {
+		case 'join':
+			words.shift();
+			var room = words.join(' ');
+			this.changeRoom(room);
+			break;
+		case 'nick':
+			words.shift();
+			var name = words.join(' ');
+			this.socket.emit('nameChange', name);
+			break;
+		default:
+			message = 'Unknown command.';
+			break;
+	}
+	return message;
+};
