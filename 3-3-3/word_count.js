aefc6724afd6767e62d67a0d269b390baf57b1dc
Chris Jazinski <chris@jazinski.com>
2014-05-25 14:08:40 -0500

    Word counter is kinda of messed up

--- 3-3-3/word_count.js
+++ 3-3-3/word_count.js
@@ -12,3 +12,36 @@ function checkIfComplete() {
 		}
 	}
 }
+
+function countWordsInText(text) {
+	var words = text
+				.toString()
+				.toLowerCase()
+				.split(/\W+/)
+				.sort();
+		for(var i in words) {
+			var word = words[i];
+			if (word) {
+				wordCounts[word] = (wordCounts[word]) ? wordCounts[word] + 1 : 1;
+			}
+		}
+}
+
+fs.readdir(fileDir, function(error, files) {
+	if (error) throw error;
+	for (var i in files) {
+		var task = (function(file) {
+			return function() {
+				fs.readFile(file, function(error, text) {
+					if (error) throw error;
+					countWordsInText(text);
+					checkIfComplete();
+				});
+			}
+		})(fileDir + '/' + files[i]);
+		tasks.push(task);
+	}
+	for (var task in tasks) {
+		tasks[task]();
+	}
+});
